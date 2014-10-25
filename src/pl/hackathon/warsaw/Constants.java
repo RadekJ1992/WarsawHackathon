919d43cef4ca0b08b2a9f224651607faa0573a8d
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2014-10-25 22:27:20 +0200

    Poprawki poprawki

--- src/pl/hackathon/warsaw/Constants.java
+++ src/pl/hackathon/warsaw/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 
     public static String appId = "766825526711978";
     public static String appSecret = "18724218ddac01255de6ea4421293f25";
-    public static String token = "CAACEdEose0cBAM5PEurcfw2tiau5del3qfOljo12WL7oyloayjSjzZBto56D9nbqKRZAQ5m54PmZCKVZCWWflhseNVNWTZBoyEH1a8QfX8Vu6ZCCO7n4C897yppRK0UPbhbzjnKcomJ6JcMpvHv7AIyfQheuGPHTlxkrrXOXRNY9gptgliNT2nxxC1vB1WGaDic0QIs4ZBkbmHdXie276ZBq";
+    public static String token = "CAACEdEose0cBAC5upzacNsRDzcI5XpAs0tcrEK1Uisz0dbV20EkkENUFr0NBf249iwKCoHo3gb6w9EwAZB4R2RAm4ZAk6VZAcJSnC1LSrllKdYWE3lW8AdeDzRX9iEYHvE9dyqY2NYKNYhzLB0HidqeSvD2oVXyBu1fH7HxYFMrtehWFjkZC7nvBz0ZAXwWeZCGMlGJs7gpAMvZA7aOmabxLPQjwLAMVJoZD";
     public static String permissions = "publish_actions,user_friends,user_about_me,user_activities,user_status,read_friendlists,user_location,email,read_mailbox,read_page_mailboxes,read_stream";
 
     
@@ -36,7 +36,7 @@ public class Constants {
     static {
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
-        cal.add(Calendar.DATE, -30);
+        cal.add(Calendar.DATE, -60);
         oldestCommunitactionDate = cal.getTime();
     }
 }
